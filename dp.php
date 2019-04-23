0f1060e9aa98e432624dcee2fa5a142d61833369
zenus <625305505@qq.com>
2019-04-23 22:52:18 +0800

    add new problem

--- dp.php
+++ dp.php
@@ -216,7 +216,28 @@
         $dp[0] = $arrs[0];
         for($i=1; $i < count($arrs); $i++)
         {
-            $dp[$i] = max($arr[0],$dp[$i-1] + $arr[0]);
+            $dp[$i] = max($arr[$i],$dp[$i-1] + $arr[$i]);
         }
+    }
+
+     public function  wordBreak()
+    {
+       给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+
+说明：
+
+拆分时可以重复使用字典中的单词。
+你可以假设字典中没有重复的单词。
+示例 1：
+
+输入: s = "leetcode", wordDict = ["leet", "code"]
+输出: true
+解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+示例 2：
+
+输入: s = "applepenapple", wordDict = ["apple", "pen"]
+输出: true
+解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+     注意你可以重复使用字典中的单词。
     }
     ?>
